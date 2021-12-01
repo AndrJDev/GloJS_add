23f1cb343ec7cc97d1ccbc27ab4d19e50e3870dc
AndreyScherbakov <and_re@mail.ru>
2021-12-01 03:12:13 +0300

    lesson04_add

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
 "use strict";
+// lesson04_add
+
+const processTheLine = function (str) {
+	let result;	
+	if (typeof str != "string") {
+		return "Это не строка";
+	}else {
+		result = str.trim();		
+		if (result.length > 30) {			
+			result = result.substring(0,30) + "...";
+		}
+		return result;
+	}   
+}
+
+console.log(processTheLine(" 123456789123456789123456789123 456789 "));
