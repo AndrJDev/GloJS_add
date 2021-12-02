68d154bf72999e50c8d6bf5bc84347ed146e95f6
AndreyScherbakov <and_re@mail.ru>
2021-12-03 02:19:11 +0300

    lesson05_2

--- script.js
+++ script.js
@@ -1,1 +1,28 @@
 "use strict";
+// lesson05 task2
+
+//Массив 
+let arr = ['62252', '99018', '15058', '49303', '98616', '29999', '12155']
+console.log(arr);
+
+for (let i = 0; i < arr.length; i++ ) {    
+    if (arr[i][0] === '2' || arr[i][0] === '4') {
+        console.log(arr[i]);
+    }           
+}
+
+//Простые числа
+function isPrime(num) {
+    if(num < 2) return false;
+    for (let i = 2; i < num; i++) {
+        if(num % i == 0)
+            return false;
+    }
+    return true;
+}
+
+for(let i = 0; i < 100; i++){
+    if(isPrime(i)) {
+        console.log(i+ " - Делители этого числа:  1 и " + i);
+    }
+}
